a59179d2943226f318ea7f191803dfe0318562d5
Will <breedon.will@gmail.com>
2023-04-20 11:22:34 +1000

    First main upload
    Core files including index.html css styling and javascript

---
+++ guildhall.js
@@ -0,0 +1,77 @@
+// codes the dark mode button to set the background grey and text white, and sets the light button to make the background white and text black. Also alternates the GH logo image and indicates which mode is currently on by changing the opacity of the svg images (darker icon is mode selected)
+
+const darkButton = document.querySelector('#dark-button');
+const lightButton = document.querySelector("#light-button");
+const regLink = document.querySelector('#register-link')
+const logo = document.querySelector('.gh-logo');
+
+function selectDark() {
+  document.body.style.backgroundColor = 'gray';
+  document.body.style.color = 'white';
+  lightButton.style.opacity = '50%';
+  lightButton.style.backgroundColor = 'gray';
+  darkButton.style.opacity = '100%';
+  darkButton.style.backgroundColor = 'gray';
+  logo.src = 'images/ghicon-dark.png';
+  logo.style.backgroundColor = 'gray';
+  regLink.style.color = 'white';
+  acknowledgePage.style.color = 'white';
+
+}
+
+function selectLight() {
+  document.body.style.backgroundColor = 'white';
+  document.body.style.color = 'black';
+  lightButton.style.opacity = '100%';
+  lightButton.style.backgroundColor = 'white';
+  darkButton.style.opacity = '30%';
+  darkButton.style.backgroundColor = 'white';
+  logo.src = 'images/ghicon.png';
+  logo.style.backgroundColor = 'white';
+  regLink.style.color = 'black';
+}
+
+darkButton.addEventListener("click", selectDark);
+lightButton.addEventListener("click", selectLight);
+
+
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirm-password');
+const submitButton = document.getElementById('submit-button');
+
+confirmPassword.addEventListener('input', function() { //input trigger on user entry of text into the confirm password field
+  if (confirmPassword.value !== password.value) {
+    confirmPassword.setCustomValidity('Passwords do not match.'); //setCustomValidity prevents form submission if passwords don't match, and presents the error message
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+});
+
+submitButton.addEventListener('click', function() {
+  if (confirmPassword.value !== password.value) {
+    confirmPassword.setCustomValidity('Passwords do not match.');
+  } else {
+    confirmPassword.setCustomValidity('');
+  }
+});
+
+
+
+  const register = document.querySelector('#register')
+  const registerCloseBtn = document.querySelector('#register-close-btn')
+  const registerTrigger = document.querySelector('#register-link')
+  const initialDisplay = "flex"
+
+  function hideRegister () {
+    register.style.display = 'none'
+    document.body.classList.remove('register-open')
+  }
+
+  function showRegister() {
+    register.style.display = initialDisplay
+    document.body.classList.add('register-open')
+  }
+
+  hideRegister()
+  registerTrigger.addEventListener('click', showRegister)
+  registerCloseBtn.addEventListener('click',hideRegister)
